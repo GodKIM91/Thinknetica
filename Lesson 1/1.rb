daf96756d2e60dc6027d1500dd8b6553b1244a81
GodKIM91 <godkimovsk@gmail.com>
2021-10-19 22:39:18 +0300

    add folders

--- Lesson 1/1.rb
+++ Lesson 1/1.rb
@@ -5,7 +5,7 @@ height = gets.chomp.to_i
 weight = (height - 110) * 1.15
 
 if weight < 0 
-    puts "Ваш вес уже оптимальный"
+  puts "Ваш вес уже оптимальный"
 else
-    puts "#{name}, Ваш идеальный вес - #{weight} кг"
+  puts "#{name}, Ваш идеальный вес - #{weight} кг"
 end
